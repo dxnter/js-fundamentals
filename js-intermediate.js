e0ff66041f0b6e66c87bd68408a22aee471ad24e
Danny <contact@danfoster.io>
2018-02-04 09:22:27 -0600

    js-scroll final

--- js-intermediate.js
+++ js-intermediate.js
@@ -1,18 +1,18 @@
 let starString = (num) => {
-    var string = "";
-    for (var i = 0; i < num; i++) {
-        string += "*"
-    }
-    return string;
+  var string = ''
+  for (var i = 0; i < num; i++) {
+    string += '*'
+  }
+  return string
 }
 
-function drawStars(arr) {
-    for (let val in arr) {
-        if (typeof arr[val] === "string") {
-            console.log(arr[val][0].toLowerCase().repeat(arr[val].length));
-            continue;
-        }
-        console.log("*".repeat(arr[val]));
+function drawStars (arr) {
+  for (let val in arr) {
+    if (typeof arr[val] === 'string') {
+      console.log(arr[val][0].toLowerCase().repeat(arr[val].length))
+      continue
     }
+    console.log('*'.repeat(arr[val]))
+  }
 }
-drawStars(["STRING", 1, 5, 6])
+drawStars(['STRING', 1, 5, 6])
---
+++ js-objects.js
@@ -0,0 +1,12 @@
+let students = [
+    {name: 'Remy', cohort: 'Jan'},
+    {name: 'Genevieve', cohort: 'March'},
+    {name: 'Chuck', cohort: 'Jan'},
+    {name: 'Osmund', cohort: 'June'},
+    {name: 'Nikki', cohort: 'June'},
+    {name: 'Boris', cohort: 'June'}
+];
+
+for (let name in students) {
+    for (let names in name) {}
+}
